b84c9746e1802d65946e8727bd9cc33dc3ce3346
Hugo-Fica <hugo.fica.saez@gmail.com>
2025-06-07 18:10:16 -0400

    modificación a get clases

--- app/api/protected/class/route.ts
+++ app/api/protected/class/route.ts
@@ -4,16 +4,21 @@ import { CreateClassRequest } from '@/types/class'
 
 export async function GET(req: NextRequest) {
   try {
-    const classChoyun = await prisma.class.findMany({
+    const classes = await prisma.class.findMany({
       select: {
         id: true,
         name: true,
         description: true,
         schedules: { select: { id: true, name: true, day: true, startTime: true, endTime: true } },
-        teacherUsers: { select: { id: true, teacher: true } },
-        studentUsers: { select: { id: true, student: true } }
+        teacherUsers: { select: { teacher: { select: { id: true } } } },
+        studentUsers: { select: { student: { select: { id: true } } } }
       }
     })
+    const classChoyun = classes.map((item) => ({
+      ...item,
+      teacherUsers: item.teacherUsers.map((t) => t.teacher),
+      studentUsers: item.studentUsers.map((s) => s.student)
+    }))
     return NextResponse.json({ classChoyun: classChoyun }, { status: 200 })
   } catch (error) {
     NextResponse.json({ message: 'Hubo un error', error: error }, { status: 500 })
